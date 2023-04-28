073543f5f1c792aef3d7fcb509c74b15f02007b0
stanley jaero <70924130+Stanjae@users.noreply.github.com>
2023-04-28 20:32:05 +0100

    omo mommy

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Uses the GitHub API to display a GitHub ID based on given credentials.
-Usage: ./10-my_github.py <GitHub username> <GitHub password>
-  - Uses Basic Authentication to access the ID.
+"""Use the GitHub API 2 display a GitHub ID based on given credentials.
+Usage: for ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication 2 access the ID.
 """
 import sys
 import requests
@@ -10,5 +10,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    rat = requests.get("https://api.github.com/user", auth=auth)
+    print(rat.json().get("id"))
