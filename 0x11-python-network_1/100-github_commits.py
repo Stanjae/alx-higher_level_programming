58561a33cef2e293adb818eb38d6aff5760a147d
stanley jaero <70924130+Stanjae@users.noreply.github.com>
2023-04-28 20:39:33 +0100

    shey na nyash

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,15 +7,15 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
+    custom_url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
+    rat = requests.get(custom_url)
+    data_in_json = rat.json()
     try:
         for i in range(10):
             print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                data_in_json[i].get("sha"),
+                data_in_json[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
