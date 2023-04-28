e269886d14db1211a017e48acddc1feb1f487519
stanley jaero <70924130+Stanjae@users.noreply.github.com>
2023-04-28 20:50:56 +0100

    omo mummy

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
+"""Sends POST request to a URL with a provided email.
+Usage: For ./6-post_email.py <URL> <email>
+  - Display the body of de response.
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    custom_url = sys.argv[1]
+    custom_value = {"email": sys.argv[2]}
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    rat = requests.post(custom_url, data=custom_value)
+    print(rat.text)
