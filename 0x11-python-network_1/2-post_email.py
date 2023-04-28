94433ad6cf6a694512de9ad56c13ee9dc3c518a3
stanley jaero <70924130+Stanjae@users.noreply.github.com>
2023-04-28 20:42:25 +0100

    omo mommy

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+"""Sends a POST request to a given URL with an email.
 Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
+  - Displays the body of the response to console.
 """
 import sys
 import urllib.parse
@@ -9,10 +9,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    custom_url = sys.argv[1]
+    custom_value = {"email": sys.argv[2]}
+    custom_data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
+    request = urllib.request.Request(custom_url, custom_data)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
