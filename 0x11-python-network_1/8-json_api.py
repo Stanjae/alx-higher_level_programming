61bd33284a57e26bae1d991d44447f60f615bb72
stanley jaero <70924130+Stanjae@users.noreply.github.com>
2023-04-28 20:57:41 +0100

    omo mommy

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
-Usage: ./8-json_api.py <letter>
+"""Makes POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: For ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+  - If null letter is provided, send `q=""`.
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    custom_letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    custom_payload = {"q": custom_letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    custom_r = requests.post("http://0.0.0.0:5000/search_user", data=custom_payload)
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        custom_response = custom_r.json()
+        if custom_response == {}:
+            print("No results Found")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(custom_response.get("id"), custom_response.get("name")))
     except ValueError:
-        print("Not a valid JSON")
+        print("Not a valid JSON Object")
