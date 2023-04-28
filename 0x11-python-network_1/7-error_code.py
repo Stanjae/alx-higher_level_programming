2a45d5b46ac7380c98e2e7f193e6f0ce4999686c
stanley jaero <70924130+Stanjae@users.noreply.github.com>
2023-04-28 20:53:23 +0100

    omo mummy

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-Usage: ./7-error_code.py <URL>
+"""Send request 2 a given URL & display de response content.
+Usage: For ./7-error_code.py <URL>
   - Handles HTTP errors.
 """
 import sys
@@ -8,10 +8,10 @@ import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    custom_url = sys.argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    custom_response = requests.get(custom_url)
+    if custom_response.status_code >= 400:
+        print("Error code: {}".format(custom_response.status_code))
     else:
-        print(r.text)
+        print(custom_response.text)
